c01f01aba37012ccd7097fac99893749dec1defa
Jamartster <jamesfoster186@gmail.com>
2025-08-06 15:39:03 -0400

    half-finished-flexbox-rules

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
-const viewButtons = document.querySelectorAll('.view-option');
+const viewButtons = document.querySelectorAll(".view-option");
 
 viewButtons.forEach((button) => {
-  button.addEventListener('click', (e) => {
+  button.addEventListener("click", (e) => {
     viewButtons.forEach((item) => {
-      if (item.classList.contains('active')) item.classList.remove('active');
-      console.log('Active statuses disabled');
+      if (item.classList.contains("active")) item.classList.remove("active");
+      console.log("Active statuses disabled");
     });
-    button.classList.add('active');
-    const itemsContainer = document.getElementById('product-container');
-    itemsContainer.removeAttribute('class');
-    itemsContainer.setAttribute('class', 'products');
+    button.classList.add("active");
+    const itemsContainer = document.getElementById("product-container");
+    itemsContainer.removeAttribute("class");
+    itemsContainer.setAttribute("class", "products");
     console.log(e.target.dataset.view);
     itemsContainer.classList.add(button.dataset.view);
   });
